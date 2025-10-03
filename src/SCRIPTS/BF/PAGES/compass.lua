473d023a0d59ecbc779732e3d4d41b12250a8d9f
PeAChristen <53952043+PeAChristen@users.noreply.github.com>
2025-10-03 15:16:02 +0200

    Create compass.lua

---
+++ src/SCRIPTS/BF/PAGES/compass.lua
@@ -0,0 +1,26 @@
+local template = assert(loadScript(radio.template))()
+local margin = template.margin
+local indent = template.indent
+local lineSpacing = template.lineSpacing
+local tableSpacing = template.tableSpacing
+local sp = template.listSpacing.field
+local yMinLim = radio.yMinLimit
+local x = margin
+local y = yMinLim - lineSpacing
+local inc = { x = function(val) x = x + val return x end, y = function(val) y = y + val return y end }
+local labels = {}
+local fields = {}
+
+labels[#labels + 1] = { t = "Compass Settings",    x = x, y = inc.y(lineSpacing) }
+fields[#fields + 1] = { t = "Magnetic Declination",     x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = 0, mult = 25, max = 20000, vals = { 4, 5 } }
+
+return {
+   read        = 133, -- MSP_COMPASS_CONFIG
+   write       = 224, -- MSP_SET_COMPASS_CONFIG
+   title       = "Compass",
+   reboot      = false,
+   eepromWrite = true,
+   minBytes    = 13,
+   labels      = labels,
+   fields      = fields
+}
