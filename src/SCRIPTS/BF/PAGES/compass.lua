bdd88c7625dc60fe76b253252fa2405c10d6fa12
PeAChristen <53952043+PeAChristen@users.noreply.github.com>
2025-10-06 11:42:22 +0200

    Update compass.lua

--- src/SCRIPTS/BF/PAGES/compass.lua
+++ src/SCRIPTS/BF/PAGES/compass.lua
@@ -11,6 +11,13 @@ local inc = { x = function(val) x = x + val return x end, y = function(val) y = 
 local labels = {}
 local fields = {}
 
+-- Load WMM.COF
+-- If not enough satelites, dispaly wait message, use betaflight min sats value
+-- If not enough satelites, user should be able to manualy enter lat, long and alt
+-- Calculate magnetic declination
+-- Let user evaluate Lat, Long, Alt and Calculated magnetic declination
+-- Let user save the new calculated magnetic delcination value
+
 labels[#labels + 1] = { t = "Compass Settings",    x = x, y = inc.y(lineSpacing) }
 fields[#fields + 1] = { t = "Magnetic Declination",     x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -30, max = 30, vals = { 1 }, scale = 0.1 }
 
