c89f0a69c4c79d72f72c818dcdea6cd7439a9907
PeAChristen <53952043+PeAChristen@users.noreply.github.com>
2025-10-06 16:28:49 +0200

    update

--- src/SCRIPTS/BF/PAGES/compass.lua
+++ src/SCRIPTS/BF/PAGES/compass.lua
@@ -22,8 +22,14 @@ local GPS_TELEM_NAME = "GPS"
 local GPS_SATS_TELEM_NAME = "Sats"
 local GPS_ALT_TELEM_NAME = "Alt"
 
+compass_page_state = 1
+_state_init = 1
+_state_gps_data = 2
+_state_manual_data = 3
+_state_manual_decl = 4
 
 -- TODO: Make user to choose from gps data or manual entered data or skip to enter magnetic decliantion directly
+-- TODO: Find out how to select _state
 -- TODO: Make gps poll loop and show num of gps sats of requierd and be able to escape back to above choice
 
 -- Load WMM.COF, if no file - display info where to put WMM.COF file /SCRIPTS/BF/COMPASS
@@ -34,7 +40,9 @@ local GPS_ALT_TELEM_NAME = "Alt"
 -- Let user save the new calculated magnetic delcination value to FC or manualy enterd value
 
 labels[#labels + 1] = { t = "Compass Settings",    x = x, y = inc.y(lineSpacing) }
-fields[#fields + 1] = { t = "Magnetic Declination",     x = x + indent, y = inc.y(lineSpacing), sp = x + sp, min = -30, max = 30, vals = { 1 }, scale = 0.1 }
+fields[#fields + 1] = { t = "Select setting option",     x = x + indent, y = inc.y(lineSpacing), sp = x + sp, upd = function (self) self.upd_num_sats_fix(slef) end }
+
+
 
 return {
    read        = 133, -- MSP_COMPASS_CONFIG
@@ -44,5 +52,11 @@ return {
    eepromWrite = true,
    minBytes    = 1,
    labels      = labels,
-   fields      = fields
+   fields      = fields,
+   function upd_num_sats_fix(self)
+      if slef.fields[1].value and self.compass_page_state = 1 then
+         self.compass_page_state = self._state_gps_data
+         slef.fields[#self.fields + 1] = { t = "Magnetic Declination",    self.x = self.x + self.indent, self.y = self.inc.y(self.lineSpacing), self.sp = self.x + self.sp, min = -30, max = 30, vals = { 1 }, scale = 0.1 }
+      end
+end
 }
